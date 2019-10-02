b4cc7c86f502d08028a99963dba5bf088c417ef6
joshua.spisak <joshs333@live.com>
2019-10-02 19:43:40 -0400

    Add lab 3/4

---
+++ lab4/sc.bash
@@ -0,0 +1,52 @@
+#!/bin/bash
+# @file sc.bash
+# @brief provides shortcuts and aliasing for running and testing the lexical analyzer
+# @author Joshua Spisak <jjs231@pitt.edu>
+export PROJECT_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export PATH="$PATH:$PROJECT_ROOT_DIR/build"
+
+##
+# @brief goes to build directory and executes the make command
+# @details creates the build directory if it does not exist and
+#  executes cmake .. if needed. Also passes all arguments to make
+function build() {
+    root_dir=$(pwd)
+    if [ ! -d ${PROJECT_ROOT_DIR}/build ]; then
+        mkdir ${PROJECT_ROOT_DIR}/build
+        cd ${PROJECT_ROOT_DIR}/build
+        jmake ..
+    fi
+
+    cd ${PROJECT_ROOT_DIR}/build
+    jmake $@
+    cd $root_dir
+}
+
+##
+# Runs the lexer against all sample data provided.
+function test() {
+    input=$1
+    verb=$2
+    if [ "$1" == "-b" ]; then
+        build
+        input=$2
+        verb=$3
+    fi
+    if [ -z "$input" ]; then
+        input="test3b.txt"
+    fi
+    #TODO(joshua.spisak): implement test script
+    root_dir=$(pwd)
+    cd ${PROJECT_ROOT_DIR}/test
+    crossword_solver DLB dict8.txt raw/$input $verb
+    cd $root_dir
+}
+
+function package() {
+    root_dir=$(pwd)
+    build
+    cp ${PROJECT_ROOT_DIR}/src/* ${PROJECT_ROOT_DIR}/build/CrosswordB
+    cd ${PROJECT_ROOT_DIR}/build/CrosswordB
+    find . -path '*/.*' -prune -o -type f -print | zip ${PROJECT_ROOT_DIR}/build/submit.zip -@
+    cd $root_dir
+}
